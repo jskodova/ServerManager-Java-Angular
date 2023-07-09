bf771400692c17fb12bf9a505cb3cddd2b4c35fe
jskodova <skodovajarmilatt@gmail.com>
2023-07-09 17:09:54 +0300

    created status enum for server

---
+++ src/main/java/app/ServerManagement/Enumeration/Status.java
@@ -0,0 +1,15 @@
+package app.ServerManagement.Enumeration;
+
+public enum Status {
+    SERVER_UP("SERVER_UP"),
+    SERVER_DOWN("SERVER_DOWN");
+    private final String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+    public String getStatus() {
+        return this.status;
+    }
+
+}
